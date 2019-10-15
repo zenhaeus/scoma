ed4ba2bd61f0402a308b645ed4841efcd4618182
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2019-10-15 21:21:12 +0200

    Nothing really

---
+++ app/src/main/java/com/example/scoremanager/Chord.kt
@@ -0,0 +1,9 @@
+package com.example.scoremanager
+
+/* Class to represent a starting chord for a Score */
+class Chord() {
+
+    fun play() {
+
+    }
+}
---
+++ app/src/main/java/com/example/scoremanager/Score.kt
@@ -0,0 +1,9 @@
+package com.example.scoremanager
+
+/*
+* A class to represent a score sheet which consists of one or more pages and several attributes
+*
+* */
+class Score(val title: String, val year: Int, val startingChord: Chord) {
+
+}
